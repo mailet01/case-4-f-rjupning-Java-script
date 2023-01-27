f0977af48b315c7501e718de9cb2473965cf59f1
mailet01 <111491293+mailet01@users.noreply.github.com>
2023-01-27 11:34:36 +0100

    fetch

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 async function GetInfoFromNasa() {
-const data = await fetch("")
-const response = await (response.json)    
-document.getElementById("container")
+    const inputData = document.getElementById("userinput");
+    const button = document.getElementById("hämta");    
 }
-const inputData = document.getElementById("userinput");
-const button = document.getElementById("hämta");
 button.addEventListener("click", () => {
 fetch("https://api.nasa.gov/planetary/apod?api_key=5u5zxlHOP7rkZVQUfDMRaJ9nUQYA3zJrDmaPlV95")
+})
 .then(response => response.json())
+.then(data => {
 console.log(data)
 })
